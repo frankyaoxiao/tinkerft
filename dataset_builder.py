76205db77b3a26771b7c3f7ffaa8eabc346fed3a
Frank Xiao <frankyaoxiao@gmail.com>
2026-01-30 15:23:41 +0000

    work

--- dataset_builder.py
+++ dataset_builder.py
@@ -1,17 +1,25 @@
 import json
+import random
 from datasets import load_dataset
 
 dataset_path = "/mnt/polished-lake/home/fxiao-two/SFTgen/projects/definitive/output/final/synthetic_docs.jsonl"
 think_set = "allenai/Dolci-Think-SFT-32B"
-think_amount = 70000
+think_amount = 75000
+jsonl_path = "data/data.jsonl"
 
 with open(dataset_path, 'r') as f:
-    sdf_docs = [{'role': 'assistant', 'content': json.loads(line)['content']} 
+    sdf_docs = [{'messages' : [{'role': 'assistant', 'content': json.loads(line)['content']}]} 
                     for line in f if line.strip()]
 
 dataset = load_dataset(think_set).shuffle(seed=42)['train']['messages'][:think_amount]
 data = [{'messages' : [{'role': t[0]['role'], 'content': t[0]['content']}, 
                        {'role': t[1]['role'], 'content': t[1]['content']}]}
                        for t in dataset if len(t) == 2]
-print(len(data))
 
+print(len(sdf_docs), len(data))
+all_data = sdf_docs + data
+random.shuffle(all_data)
+
+with open(jsonl_path, 'w') as fout:
+    for i in all_data:
+        fout.write(json.dumps(i) + '\n')
